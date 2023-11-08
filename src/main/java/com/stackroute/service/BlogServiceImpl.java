399c7d75e52036f48bdaada64356f8129da1ca87
Rohit <rohit.1.kumar@stackroute.in>
2023-11-08 14:59:46 +0530

    init

---
+++ src/main/java/com/stackroute/service/BlogServiceImpl.java
@@ -0,0 +1,11 @@
+package com.stackroute.service;
+
+import org.springframework.stereotype.Service;
+/* This is ServiceImplementation Class which should implement BlogService Interface and override all the implemented methods.
+ * Handle suitable exceptions for all the implemented methods*/
+
+@Service
+public class BlogServiceImpl  {
+
+}
+
